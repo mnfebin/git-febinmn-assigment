3cae76aebcc17b70e1247ed856f274851c5824ec
febin mn <febin9778@gmail.com>
2025-09-22 14:03:31 +0530

    add temp.txt

---
+++ febin.py
@@ -0,0 +1,11 @@
+print("hello febin 🎩")
+print("helloworld")
+
+# Taking integer input
+age = int(input("Enter your age: "))
+
+# Taking string input
+name = input("Enter your name: ")
+
+print("Your name is:", name)
+print("Your age is:", age)
